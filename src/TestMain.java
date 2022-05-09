b596423505f1f3ba6a131ec902d124b02ba7019c
sullbeans <105225410+sullbeans@users.noreply.github.com>
2022-05-09 13:47:25 -0400

    Adding Main file
    this is a test

---
+++ src/TestMain.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class TestMain {
+	
+	public TestMain() throws IOException
+	{
+		System.out.println("heheheha");
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		new TestMain();
+	}
+}
