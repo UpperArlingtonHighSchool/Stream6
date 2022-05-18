41c27c562c7cd5283f618c0c8a08bf9a5f817073
sullbeans <105225410+sullbeans@users.noreply.github.com>
2022-05-18 14:01:44 -0400

    stream balance patch >:)

--- stream6/src/main/java/Main.java
+++ stream6/src/main/java/Main.java
@@ -1,16 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+// import org.jfree.chart.ChartFactory;
+import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYDataset;
 
 /**
  * Start file
  */
 public class Main {
 	
+	private XYDataset tempXYData;
+	private ChartDrawer testDrawer;
+	
 	public Main() throws IOException
 	{
-		JFrame frame = new JFrame("Willy");
+		JFrame frame = new JFrame("Stream6 Project");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// This centers the JFrame to the screen
 		frame.setSize(500, 400);
@@ -18,10 +28,55 @@ public class Main {
 		// This is necessary to allow JFrame elements to be placed freely
 		frame.setLayout(null);
 		
+		JTextArea area = new JTextArea("This is where we flex");
+		area.setBounds(5, 5, 200, 300);
+		area.setEditable(false);
+		frame.getContentPane().add(area);
+		
 		// Example element
-		JButton test = new JButton("button");
-		test.setBounds(50, 100, 100, 50);
+		JButton test = new JButton("Open Graph");
+		test.setBounds(225, 25, 100, 50);
+		test.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				JFrame graph = new JFrame("Graph");
+				graph.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				// This centers the JFrame to the screen
+				graph.setSize(300, 300);
+				graph.setLocationRelativeTo(null);
+				// This is necessary to allow JFrame elements to be placed freely
+				graph.setLayout(null);
+				
+				// Let's temporarily create an xy dataset
+				double[][] dataTemp = new double[2][20];
+				for(int i = 0; i < dataTemp[0].length; i++)
+				{
+					dataTemp[0][i] = i;
+					dataTemp[1][i] = Math.random() * 10;
+				}
+				
+				DefaultXYDataset dataset = new DefaultXYDataset();
+				dataset.addSeries("AMOGUS", new Statistics().getData());
+				tempXYData = dataset;
+				
+				testDrawer = new ChartDrawer(graph, tempXYData, "AMOGUS", "X", "Y");
+				testDrawer.drawGraph();
+				testDrawer.updateGraph(tempXYData);
+				
+				graph.setVisible(true);
+			}
+		});
 		frame.getContentPane().add(test);
+		
+		// Let's temporarily create an xy dataset
+		double[][] dataTemp = new double[2][20];
+		
+		for(int i = 0; i < dataTemp[0].length; i++)
+		{
+			dataTemp[0][i] = i;
+			dataTemp[1][i] = Math.random() * 10;
+		}
 		
 		// Call this after adding all JFrame elements
 		frame.setVisible(true);
--- stream6/src/main/java/Statistics.java
+++ stream6/src/main/java/Statistics.java
@@ -1,27 +1,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-/**
-Reads stream information from the AllTheDataCorrected.txt file
-**/
 public class Statistics {
 	
-	public ArrayList<String> location = new ArrayList();
-	public ArrayList<String> date = new ArrayList();
-	public ArrayList<Double> pH = new ArrayList();
-	public ArrayList<Double> nitrate = new ArrayList();
-	public double pHMean ,pHStDev, pHPValue, nitrateMean, nitrateStDev, nitratePValue;
+	public ArrayList<String> location = new ArrayList<String>();
+	public ArrayList<String> date = new ArrayList<String>();
+	public ArrayList<Double> pH = new ArrayList<Double>();
+	public ArrayList<Double> nitrate = new ArrayList<Double>();
+	public double pHMean, pHStDev, nitrateMean, nitrateStDev;
+	public double[][] pHvsNitrate = new double[2][20];
 	
-	public Statistics() throws FileNotFoundException {
-		getData();
-	}
-	//reads the data from the data file and puts the interested data into array lists
-	public void getData() throws FileNotFoundException {
+	// reads the data from the data file and puts the interested data into array lists
+	private void updateData() throws FileNotFoundException
+	{
 		Scanner read = new Scanner(new File("src/main/resources/AllTheDataCorrected.txt"));
-		int hi = 0;
-		while(read.hasNextLine()) {
+		
+		while(read.hasNextLine())
+		{
 			location.add(read.next());
 			date.add(read.next());
 			read.nextDouble();
@@ -31,40 +29,84 @@ public class Statistics {
 			read.nextDouble();
 			read.nextDouble();
 			read.nextDouble();
+			read.nextDouble();
+			read.nextDouble();
+			read.nextDouble();
 			pH.add(read.nextDouble());
 			read.nextDouble();
 			read.nextDouble();
 			read.nextDouble();
-			read.nextDouble();
-			read.nextDouble();
-			read.nextDouble();
-			hi++;
 		}
+		
 		System.out.println("Locations: " + location + "\nDates: " + date + "\npH: " + pH + "\nNitrate: " + nitrate);
 		oneVarStat("pH");
 		oneVarStat("Nitrate");
+		setMatrix();
 	}
 	
-	public void oneVarStat(String data) {
-		if (data.equals("pH")) {
-			ArrayList<Double> tempPH = pH;
-			for(int i = tempPH.size() - 1; i >= 0; i--) {
+	private void setMatrix()
+	{
+		ArrayList<Double> tempPH = new ArrayList<Double>(pH);
+		ArrayList<Double> tempNitrate = new ArrayList<Double>(nitrate);
+		int spot = 0;
+		for(int i = tempPH.size() - 1; i >= 0; i--)
+		{
+			pHvsNitrate[0][spot] = tempPH.remove(i);
+			pHvsNitrate[1][spot] = tempNitrate.remove(i);
+			spot++;
+		}
+		
+		for(double[] column : pHvsNitrate)
+		{
+			System.out.println(Arrays.toString(column));
+		}
+		
+	}
+	
+	private void oneVarStat(String data)
+	{
+		if(data.equals("pH"))
+		{
+			ArrayList<Double> tempPH = new ArrayList<Double>(pH);
+			for(int i = tempPH.size() - 1; i >= 0; i--)
+			{
 				pHMean += tempPH.remove(i);
 			}
 			pHMean /= 20;
-			System.out.println(pHMean);
+			tempPH = new ArrayList<Double>(pH);
+			for(int i = tempPH.size() - 1; i >= 0; i--)
+			{
+				pHStDev += Math.pow((tempPH.remove(i) - pHMean), 2);
+			}
+			pHStDev = Math.sqrt(pHStDev /= 20);
+			System.out.println(pHMean + "\n" + pHStDev);
 		}
-		else if(data.equals("Nitrate")) {
-			ArrayList<Double> tempNitrate = nitrate;
-			for(int i = tempNitrate.size() - 1; i >= 0; i--) {
+		else if(data.equals("Nitrate"))
+		{
+			ArrayList<Double> tempNitrate = new ArrayList<Double>(nitrate);
+			for(int i = tempNitrate.size() - 1; i >= 0; i--)
+			{
 				nitrateMean += tempNitrate.remove(i);
 			}
 			nitrateMean /= 20;
-			System.out.println(nitrateMean);
+			tempNitrate = new ArrayList<Double>(nitrate);
+			for(int i = tempNitrate.size() - 1; i >= 0; i--)
+			{
+				nitrateStDev += Math.pow((tempNitrate.remove(i) - nitrateMean), 2);
+			}
+			nitrateStDev = Math.sqrt(nitrateStDev /= 20);
+			System.out.println(nitrateMean + "\n" + nitrateStDev);
 		}
 	}
 	
-	public void twoVarStat() {
+	public double[][] getData()
+	{
+		try {
+			updateData();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
+		return pHvsNitrate;
 	}
 }
