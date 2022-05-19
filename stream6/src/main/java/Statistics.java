4d39f11653f1a5b2f4030506e442978787e64e36
James Dunsizer <105236447+DeathBot773@users.noreply.github.com>
2022-05-19 13:39:43 -0400

    Zachary and James won't going to be here on Monday

--- stream6/src/main/java/Statistics.java
+++ stream6/src/main/java/Statistics.java
@@ -6,24 +6,27 @@ import java.util.Scanner;
 
 public class Statistics {
 	
-	public ArrayList<String> location = new ArrayList<String>();
-	public ArrayList<String> date = new ArrayList<String>();
-	public ArrayList<Double> pH = new ArrayList<Double>();
-	public ArrayList<Double> nitrate = new ArrayList<Double>();
-	public double pHMean, pHStDev, nitrateMean, nitrateStDev;
+	public ArrayList<String> location = new ArrayList();
+	public ArrayList<Integer> date = new ArrayList();
+	public ArrayList<Double> pH = new ArrayList();
+	public ArrayList<Double> nitrate = new ArrayList();
+	public double pHMean ,pHStDev, nitrateMean, nitrateStDev;
 	public double[][] pHvsNitrate = new double[2][20];
+	public double[][] oneVarGraphPH = new double[2][20];
+	public double[][] oneVarGraphNitrate = new double[2][20];
 	
-	// reads the data from the data file and puts the interested data into array lists
-	private void updateData() throws FileNotFoundException
-	{
+	public Statistics() throws FileNotFoundException {
+		getData();
+	}
+	
+	//reads the data from the data file and puts the interested data into array lists
+	public void getData() throws FileNotFoundException {
 		Scanner read = new Scanner(new File("src/main/resources/AllTheDataCorrected.txt"));
-		
-		while(read.hasNextLine())
-		{
+		while(read.hasNextLine()) {
 			location.add(read.next());
-			date.add(read.next());
+			date.add(read.nextInt());
 			read.nextDouble();
-			nitrate.add(read.nextDouble());
+			read.nextDouble();
 			read.nextDouble();
 			read.nextDouble();
 			read.nextDouble();
@@ -33,24 +36,22 @@ public class Statistics {
 			read.nextDouble();
 			read.nextDouble();
 			pH.add(read.nextDouble());
-			read.nextDouble();
+			nitrate.add(read.nextDouble());
 			read.nextDouble();
 			read.nextDouble();
 		}
-		
 		System.out.println("Locations: " + location + "\nDates: " + date + "\npH: " + pH + "\nNitrate: " + nitrate);
 		oneVarStat("pH");
 		oneVarStat("Nitrate");
 		setMatrix();
 	}
 	
-	private void setMatrix()
-	{
-		ArrayList<Double> tempPH = new ArrayList<Double>(pH);
-		ArrayList<Double> tempNitrate = new ArrayList<Double>(nitrate);
+	//sets the 2D-Array of pH and nitrate for use in line graphs
+	public void setMatrix() {
+		ArrayList<Double> tempPH = new ArrayList(pH);
+		ArrayList<Double> tempNitrate = new ArrayList(nitrate);
 		int spot = 0;
-		for(int i = tempPH.size() - 1; i >= 0; i--)
-		{
+		for(int i = tempPH.size() - 1; i >= 0; i--) {
 			pHvsNitrate[0][spot] = tempPH.remove(i);
 			pHvsNitrate[1][spot] = tempNitrate.remove(i);
 			spot++;
@@ -63,50 +64,67 @@ public class Statistics {
 		
 	}
 	
-	private void oneVarStat(String data)
-	{
-		if(data.equals("pH"))
-		{
-			ArrayList<Double> tempPH = new ArrayList<Double>(pH);
-			for(int i = tempPH.size() - 1; i >= 0; i--)
-			{
+	//finds the mean and standard deviation of the data for either pH or nitrate
+	public void oneVarStat(String data) {
+		//finds the mean and standard deviation of the pH
+		if (data.equals("pH")) {
+			ArrayList<Double> tempPH = new ArrayList(pH);
+			for(int i = tempPH.size() - 1; i >= 0; i--) {
 				pHMean += tempPH.remove(i);
 			}
 			pHMean /= 20;
-			tempPH = new ArrayList<Double>(pH);
-			for(int i = tempPH.size() - 1; i >= 0; i--)
-			{
+			tempPH = new ArrayList(pH);
+			for(int i = tempPH.size() - 1; i >= 0; i--) {
 				pHStDev += Math.pow((tempPH.remove(i) - pHMean), 2);
 			}
 			pHStDev = Math.sqrt(pHStDev /= 20);
 			System.out.println(pHMean + "\n" + pHStDev);
-		}
-		else if(data.equals("Nitrate"))
-		{
-			ArrayList<Double> tempNitrate = new ArrayList<Double>(nitrate);
-			for(int i = tempNitrate.size() - 1; i >= 0; i--)
+			
+			tempPH = new ArrayList(pH);
+			ArrayList<Integer> tempYear = new ArrayList(date);
+			int spot = 0;
+			for(int i = tempPH.size() - 1; i >= 0; i--) {
+				oneVarGraphPH[1][spot] = tempPH.remove(i);
+				oneVarGraphPH[0][spot] = tempYear.remove(i);
+				spot++;
+			}
+			for(double[] column : oneVarGraphPH)
 			{
+				System.out.println(Arrays.toString(column));
+			}
+		}
+		//finds the mean and standard deviation of the nitrate
+		else if(data.equals("Nitrate")) {
+			ArrayList<Double> tempNitrate = new ArrayList(nitrate);
+			for(int i = tempNitrate.size() - 1; i >= 0; i--) {
 				nitrateMean += tempNitrate.remove(i);
 			}
 			nitrateMean /= 20;
-			tempNitrate = new ArrayList<Double>(nitrate);
-			for(int i = tempNitrate.size() - 1; i >= 0; i--)
-			{
+			tempNitrate = new ArrayList(nitrate);
+			for(int i = tempNitrate.size() - 1; i >= 0; i--) {
 				nitrateStDev += Math.pow((tempNitrate.remove(i) - nitrateMean), 2);
 			}
 			nitrateStDev = Math.sqrt(nitrateStDev /= 20);
 			System.out.println(nitrateMean + "\n" + nitrateStDev);
+			
+			tempNitrate = new ArrayList(nitrate);
+			ArrayList<Integer> tempYear = new ArrayList(date);
+			int spot = 0;
+			for(int i = tempNitrate.size() - 1; i >= 0; i--) {
+				oneVarGraphNitrate[1][spot] = tempNitrate.remove(i);
+				oneVarGraphNitrate[0][spot] = tempYear.remove(i);
+				spot++;
+			}
+			for(double[] column : oneVarGraphNitrate)
+			{
+				System.out.println(Arrays.toString(column));
+			}
+			System.out.println("boo");
 		}
+
 	}
 	
-	public double[][] getData()
-	{
-		try {
-			updateData();
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
+	public void twoVarStat() {
 		
-		return pHvsNitrate;
 	}
 }
